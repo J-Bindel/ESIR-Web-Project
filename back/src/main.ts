9bb4dddf0de1a1b3df95f3667b8d5d8e514dbcb8
Jérémy Bindel <jeremy.bindel@etudiant.univ-rennes1.fr>
2024-04-19 22:03:13 +0200

    Translation + allow CORS

--- back/src/main.ts
+++ back/src/main.ts
@@ -5,10 +5,13 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4200',
+  });
   app.use(helmet());
   const config = new DocumentBuilder()
-    .setTitle('Gestion des Associations')
-    .setDescription('Descriptions des APIs de la gestion des associations')
+    .setTitle('Associations management')
+    .setDescription('API for managing associations and users.')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
