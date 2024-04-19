ef6796bbb3e6812375b77a4c891f55ef36a8851e
Jérémy Bindel <jeremy.bindel@etudiant.univ-rennes1.fr>
2024-04-19 22:05:38 +0200

    Add new styles for the login logic

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { UsersListComponent } from './users-list/users-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
@@ -22,6 +23,7 @@ import { NavComponent } from './nav/nav.component';
     BrowserAnimationsModule,
     MatTableModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [
     {
--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ApiHelperService } from '../services/api-helper.service';
 import { TokenStorageService } from '../services/token-storage.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,26 +9,38 @@ import { TokenStorageService } from '../services/token-storage.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  
-  public errorMessage: string;
+
   constructor(
+
     private api: ApiHelperService,
     private tokenStorageService: TokenStorageService
   ) {
-    this.errorMessage = '';
    }
+  email: string = '';
+  showPasswordInput: boolean = false;
+  errorMessage = '';
+
   login(): void {
     const username: string = (document.getElementById('username') as HTMLInputElement).value;
     const password: string = (document.getElementById('password') as HTMLInputElement).value;
     this.api.post({ endpoint: '/auth/login', data: { username, password } }).then(response => {
       this.tokenStorageService.save(response.access_token);
-      window.location.href = '/users';
-    }
-    ).catch(error => console.log(error));
-    if (this.tokenStorageService.isLogged()) {
+      
+      if (this.tokenStorageService.isLogged()) {
         window.location.href = '/users';
-    }else {
-      this.errorMessage = 'Wrong username or password';
+      }else{
+        this.errorMessage = 'Wrong username or password';
+      }
+    }
+    ).catch(error => {
+      console.log(error);
+      this.errorMessage = 'An error occurred during login';
+    });
+  }
+
+  showPassword() {
+    if (this.email.trim() !== '') {
+      this.showPasswordInput = true;
     }
   }
 }
