e08d2c293e9504b2bf9aa20b352a46e757b69434
Jérémy Bindel <jeremy.bindel@etudiant.univ-rennes1.fr>
2024-04-28 21:53:00 +0200

    Add Material checkbox

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { UsersListComponent } from './users-list/users-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { LoginComponent } from './login/login.component';
@@ -24,6 +25,7 @@ import { NavComponent } from './nav/nav.component';
     BrowserAnimationsModule,
     MatTableModule,
     MatPaginatorModule,
+    MatCheckboxModule,
     HttpClientModule,
     FormsModule
   ],
