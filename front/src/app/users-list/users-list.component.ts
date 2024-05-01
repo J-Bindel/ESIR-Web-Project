755db9ba0a8feb581829d5cf22d204f0d1e85d02
Jérémy Bindel <jeremy.bindel@etudiant.univ-rennes1.fr>
2024-05-01 16:42:33 +0200

    New component taking into account, new table column, new error message

--- front/src/app/users-list/users-list.component.ts
+++ front/src/app/users-list/users-list.component.ts
@@ -3,7 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
+import { UserEditPopupComponent } from '../user-edit-popup/user-edit-popup.component';
   
   @Component({
     selector: 'app-users-list',
@@ -12,15 +14,17 @@ import { Observable } from 'rxjs';
   })
   
   export class UsersListComponent implements AfterViewInit {
-    displayedColumns: string[] = ['select', 'id', 'lastname', 'firstname', 'age'];
+    displayedColumns: string[] = ['select', 'id', 'lastname', 'firstname', 'age', 'e-mail'];
     dataSource = new MatTableDataSource<User>();
     selectedRows: SelectionModel<User> = new SelectionModel<User>(true, []);
+    editErrorMessage = '';
 
 
     @ViewChild(MatPaginator) paginator: MatPaginator;
 
     constructor( 
-      private http: HttpClient
+      private http: HttpClient,
+      public dialog: MatDialog
     ) {}
     ngAfterViewInit(): void {
       this.dataSource.paginator = this.paginator;
@@ -46,7 +50,25 @@ import { Observable } from 'rxjs';
     fetchData(): Observable<User[]> {
       return this.http.get<User[]>('http://localhost:3000/users');
     }
+    
+    getSelectedUsersCount(): number {
+      return this.selectedRows.selected.length;
+    }
 
+    openUserEditPopup(selectedUser: User): void {
+      if (this.getSelectedUsersCount() !== 1) {
+        this.editErrorMessage = 'Select exactly one user when editing';
+        return;
+      }
+
+      const dialogRef = this.dialog.open(UserEditPopupComponent, {
+        data: { user: selectedUser }
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        console.log('The dialog was closed');
+      });
+    }
 }
 
 export interface User {
@@ -54,4 +76,5 @@ export interface User {
   lastname: string;
   firstname: string;
   age: number;
+  email: string;
 }
