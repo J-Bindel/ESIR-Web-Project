625903781a7433768a606b0129fdf993ca3af260
Jérémy Bindel <jeremy.bindel@etudiant.univ-rennes1.fr>
2024-05-01 11:59:18 +0200

    Change checkstyle + add new method to the user serivce

--- back/src/users/users.service.ts
+++ back/src/users/users.service.ts
@@ -9,36 +9,40 @@ export class UsersService {
     
     constructor(
         @InjectRepository(User)
-        private repository : Repository<User>
+        private repository: Repository<User>
     ) { }
 
-    public getAllUsers() : Promise <User[]> {
+    public getAllUsers(): Promise <User[]> {
         return this.repository.find();
     }
 
-    public async getUserById(id : number) : Promise <User> {
+    public async getUserById(id: number): Promise <User> {
          return await this.repository.findOne({where: {id: Equal(id)}});
-     }
+    }
+
+    public async getUserByEmail(email: string): Promise <User> {
+        return await this.repository.findOne({where: {email: Equal(email)}});
+    }
      
-    public async create(firstname : string, lastname : string, age : number, email : string, password : string) : Promise <User> {
+    public async create(firstname: string, lastname: string, age: number, email: string, password: string): Promise <User> {
         const saltOrRounds = 10;
         const hash = await bcrypt.hash(password, saltOrRounds);
-         const newUser : User = await this.repository.create ({
-            firstname : firstname,
-            lastname : lastname,
-            age : age,
-            email : email,
-            password : hash
+         const newUser: User = await this.repository.create ({
+            firstname: firstname,
+            lastname: lastname,
+            age: age,
+            email: email,
+            password: hash
          })
          await this.repository.save(newUser);
          return newUser;
      }
      
-    public async setUser(id : number, firstname : string, lastname : string, age : number, email: string, password : string) : Promise <User>{
+    public async setUser(id: number, firstname: string, lastname: string, age: number, email: string, password: string): Promise <User>{
          if (firstname !== undefined && lastname !== undefined && age !== undefined && email !== undefined && password !== undefined) {
             const saltOrRounds = 10;
             const hash = await bcrypt.hash(password, saltOrRounds);
-            const user : User = await this.repository.findOne({where: {id: Equal(id)}});
+            const user: User = await this.repository.findOne({where: {id: Equal(id)}});
             user.firstname = firstname;
             user.lastname = lastname;
             user.age = age;
@@ -50,8 +54,8 @@ export class UsersService {
          return undefined;
      }
       
-    public async deleteUser(id : number) : Promise <boolean>{
-        const user : User = await this.repository.findOne({where: {id: Equal(id)}});
+    public async deleteUser(id: number) : Promise <boolean>{
+        const user: User = await this.getUserById(id);
         await this.repository.delete(user);
         return true;
      }
