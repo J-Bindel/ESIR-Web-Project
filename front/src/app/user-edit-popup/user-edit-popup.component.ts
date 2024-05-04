5d644de51fb1ad70408fc09a9b54058ce91ddb53
Jérémy BINDEL <jerebind@outlook.com>
2024-05-04 11:57:00 +0200

    Update user-edit-popup component: new error handling and add backend put method

--- front/src/app/user-edit-popup/user-edit-popup.component.ts
+++ front/src/app/user-edit-popup/user-edit-popup.component.ts
@@ -1,8 +1,9 @@
 import { Component, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { NgForm } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { User } from '../users-list/users-list.component';
+import { ApiHelperService } from '../services/api-helper.service';
 
 @Component({
   selector: 'app-user-edit-popup',
@@ -11,13 +12,34 @@ import { User } from '../users-list/users-list.component';
 })
 export class UserEditPopupComponent {
   user: User;
+
+  // Define index signature
+  [key: string]: any;
+
+  // Define form controls for each input field
+  lastname = new FormControl('', [Validators.required]);
+  firstname = new FormControl('', [Validators.required]);
+  age = new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$')]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  errorMessage: {[key: string]: any}  = { 
+    lastname: '',
+    firstname: '',
+    age: '',
+    email: '',
+  };
   
   constructor(
     public dialogRef: MatDialogRef<UserEditPopupComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { user: User },
-    private http: HttpClient,
+    private api: ApiHelperService,
   ) {
     this.user = data.user;
+
+     this.lastname.valueChanges.subscribe(() => this.updateErrorMessage('lastname'));
+     this.firstname.valueChanges.subscribe(() => this.updateErrorMessage('firstname'));
+     this.age.valueChanges.subscribe(() => this.updateErrorMessage('age'));
+     this.email.valueChanges.subscribe(() => this.updateErrorMessage('email'));
   }
 
   close(): void {
@@ -26,10 +48,28 @@ export class UserEditPopupComponent {
 
   onSubmit(form: NgForm): void {
     console.log(`http://localhost:3000/users/${this.user.id}`, form.value)
-  //   this.http.put(`http://localhost:3000/users/${this.user.id}`, form.value)
-  //   .subscribe(response => {
-  //     console.log('User updated successfully', response);
-  // });
-  // this.dialogRef.close();
+    this.api.put({ endpoint: '/users/', data: { lastname: this.user.lastname, firstname: this.user.firstname, age: this.user.age, email: this.user.email }, queryParams: { id: this.user.id }})
+      .then(() => {
+        this.dialogRef.close();
+        window.location.href = '/users'; 
+      }).catch(error => {
+        console.log(error);
+      });
   }
+
+  updateErrorMessage(fieldName: string) {
+    const control = this[fieldName];
+    if (control.invalid && control.dirty) {
+      if (control.hasError('required')) {
+        this.errorMessage[fieldName] = 'This field is required';
+      } else if (control.hasError('pattern')) {
+        this.errorMessage[fieldName] = 'Please enter a valid number';
+      } else if (control.hasError('email')) {
+        this.errorMessage[fieldName] = 'Please enter a valid email';
+      }
+    } else {
+      this.errorMessage[fieldName] = '';
+    }
+  }
+
 }
