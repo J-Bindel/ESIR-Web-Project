7bb62adfcfb41ea1cd1a915765817b3b23b6aa60
Jérémy BINDEL <jerebind@outlook.com>
2024-05-06 19:43:19 +0200

    Distinguish UserLogin and AssociationLogin

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { ApiBody, ApiCreatedResponse, ApiOperation, ApiProperty, ApiTags } from '@nestjs/swagger';
 
-export class AuthInput {
+export class UserLogin {
 
   @ApiProperty({
     description: 'The email of the user',
@@ -18,8 +18,27 @@ export class AuthInput {
     type: String,
   })
   public password: string;
+
 }
-  
+
+export class AssociationLogin {
+
+  @ApiProperty({
+    description: 'The id of the association',
+    example: 1,
+    type: Number,
+  })
+  public id: number;
+
+  @ApiProperty({
+    description: 'The password of the association',
+    example: 'valid_password',
+    type: String,
+  })
+  public password: string;
+
+}
+
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
@@ -32,19 +51,41 @@ export class AuthController {
       description : 'The user has been successfully authenticated.'
     })
     @ApiBody({
-      type: AuthInput,
+      type: UserLogin,
       description: "A user email and a password",
       examples: {
           a: {
               summary: "john.doe@example.com valid_password",
               description: "User example",
-              value: {email: 'john.doe@example.com', password:"valid_password"} as AuthInput
+              value: {email: 'john.doe@example.com', password:"valid_password"}
+          }
+      }
+    })
+    @UseGuards(AuthGuard('local'))
+    @Post('user/login')
+    async userLogin(@Request() request) {
+      return this.authService.userLogin(request.user);
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @ApiOperation({ summary: 'Authenticate a user to edit association'})
+    @ApiCreatedResponse({
+      status: 200,
+      description : 'The user has been successfully authenticated to modify the association.'
+    })
+    @ApiBody({
+      type: AssociationLogin,
+      description: "An association id and a password",
+      examples: {
+          a: {
+              summary: "1 valid_password",
+              description: "Association id and password pair example",
+              value: {email: 1 , password:"valid_password"}
           }
       }
   })
-    @UseGuards(AuthGuard('local'))
-    @Post('login')
-    async login(@Request() request) {
-      return this.authService.login(request.user);
+    @Post('association/login')
+    async associationLogin(@Request() request) {
+      return this.authService.validateAssociation(request.association.id, request.association.password);
     }
 }
