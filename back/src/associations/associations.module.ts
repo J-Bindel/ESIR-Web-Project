823371c6c241afbf741a68050daf68521cc4d0a4
Jérémy BINDEL <jerebind@outlook.com>
2024-05-14 22:53:33 +0200

    Replace the User list by a user id string in the Association. Need the UserRepository to add that users existing when creating

--- back/src/associations/associations.module.ts
+++ back/src/associations/associations.module.ts
@@ -4,9 +4,10 @@ import { UsersModule } from 'src/users/users.module';
 import { Association } from './association.entity';
 import { AssociationsController } from './associations.controller';
 import { AssociationsService } from './associations.service';
+import { User } from 'src/users/user.entity';
 
 @Module({
-    imports : [TypeOrmModule.forFeature([Association]), UsersModule],
+    imports : [TypeOrmModule.forFeature([User, Association]), UsersModule],
     controllers:[AssociationsController],
     providers: [AssociationsService],
     exports: [AssociationsService]
--- back/src/associations/associations.service.ts
+++ back/src/associations/associations.service.ts
@@ -1,67 +1,100 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { User } from 'src/users/user.entity';
-import { UsersService } from 'src/users/users.service';
 import { Association } from './association.entity';
+import { UsersModule } from 'src/users/users.module';
 import { Equal, Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
+import { InjectRepository, TypeOrmModule } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
 
+@Module({
+    imports: [TypeOrmModule.forFeature([Association, User]),
+    UsersModule],
+})
 @Injectable()
 export class AssociationsService {
     
     constructor(
         @InjectRepository(Association)
-        private repository: Repository <Association>,
-        private service: UsersService
+        private assoRepository: Repository <Association>,
+        @InjectRepository(User)
+        private userRepository: Repository <User>
     ) {}
     
     public async getAllAssos(): Promise <Association[]> {
-        return this.repository.find();
+        return this.assoRepository.find();
     }
 
     public async getAssoById(id : number): Promise <Association> {
-        return await this.repository.findOne({where: {id: Equal(id)}});
+        return await this.assoRepository.findOne({where: {id: Equal(id)}});
      }
      
     public async create(idUsers: number[], name: string, password: string): Promise <Association> {
         const saltOrRounds = 10;
         const hash = await bcrypt.hash(password, saltOrRounds);
-        const users: User[] = [];
-        idUsers.forEach(async id => {users.push(await this.service.getUserById(id))});
-        const newAssociation: Association = await this.repository.create({
-            users: users, 
+        idUsers.forEach(async (id) => {
+            const user: User = await this.userRepository.findOne({where: {id: Equal(id)}});
+            if (user === undefined) {
+                return undefined;
+            }
+        });
+        if (name === undefined || password === undefined) {
+            return undefined;
+        }
+        // Transform the array of user ids into a string
+        const idUsersString = idUsers.join(',');
+        const newAssociation: Association = await this.assoRepository.create({
+            userIds: idUsersString, 
             name: name,
             password: hash
         });
-        await this.repository.save(newAssociation);
+        await this.assoRepository.save(newAssociation);
         return newAssociation;
      }
      
     public async setAsso(id: number, idUsers: number [], name: string, password: string): Promise <Association> {
+        idUsers.forEach(async (id) => {
+            const user: User = await this.userRepository.findOne({where: {id: Equal(id)}});
+            if (user === undefined) {
+                return undefined;
+            }
+        });
+        if (name === undefined || password === undefined) {
+            return undefined;
+        }
         const saltOrRounds = 10;
         const hash = await bcrypt.hash(password, saltOrRounds);
-        if (idUsers !== undefined && name !== undefined) {
-            const users: User [] = [];
-            idUsers.forEach(async id => {users.push(await this.service.getUserById(id))});
-            const asso: Association = await this.repository.findOne({where: {id: Equal(id)}});
-            asso.users = users;
-            asso.name = name;
-            asso.password = hash;
-            await this.repository.save(asso);
-            return asso;
+        const idUsersString = idUsers.join(',');
+        const asso: Association = await this.assoRepository.findOne({where: {id: Equal(id)}});
+        if (asso === undefined) {
+            return undefined;
         }
-        return undefined;
-     }
+        asso.userIds = idUsersString;
+        asso.name = name;
+        asso.password = hash;
+        await this.assoRepository.save(asso);
+        return asso;
+    }
       
-    public async deleteAsso(id : number) : Promise <boolean> {
-        const asso: Association = await this.repository.findOne({where: {id: Equal(id)}});
-        await this.repository.delete(asso);
-        return true;
+    public async deleteAsso(id: number): Promise <boolean> {
+        try {
+            const result = await this.assoRepository.delete(id);
+            return result.affected > 0;
+        } catch(error) {
+            console.error('Error deleting association: ', error);   
+            return false;
+        }
      }
 
      public async getMembers(id: number): Promise <User[]> {
-        const asso: Association = await this.repository.findOne({where: {id: Equal(id)}});
-        return asso.users;
+        const asso: Association = await this.assoRepository.findOne({where: {id: Equal(id)}});
+        const members: User[] = [];
+        // Transform the string of user ids into an array
+        const userIds = asso.userIds.split(',');
+        userIds.forEach(async (id) => {
+            const user: User = await this.userRepository.findOne({where: {id: Equal(+id)}});
+            members.push(user);
+        });
+        return members;
      }
 
 }
