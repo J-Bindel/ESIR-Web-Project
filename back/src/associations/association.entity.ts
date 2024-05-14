62c65b939f7ae55b9f495a438dcb77805e72a3bb
Jérémy BINDEL <jerebind@outlook.com>
2024-05-14 22:51:47 +0200

    Replace the User list by a user id string in the Association

--- back/src/associations/association.entity.ts
+++ back/src/associations/association.entity.ts
@@ -1,13 +1,11 @@
-import { User } from "src/users/user.entity";
-import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Association {
     @PrimaryGeneratedColumn()
     public id : number;
-    @ManyToMany(type => User, { eager: true })
-    @JoinTable()
-    public users : User[];
+    @Column()
+    public userIds : string;
     @Column()
     public name : string;
     @Column()
