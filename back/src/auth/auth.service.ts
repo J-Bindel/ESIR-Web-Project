23867e76b99db193ad133063b27d3afe8f9aaf2c
Jérémy BINDEL <jerebind@outlook.com>
2024-05-24 20:10:08 +0200

    Provide user information when credentials are correct

--- back/src/auth/auth.service.ts
+++ back/src/auth/auth.service.ts
@@ -49,6 +49,7 @@ export class AuthService {
         const payload = { username: user.id };
         return {
             access_token: this.jwtService.sign(payload),
+            user: user,
         };
     }
 
