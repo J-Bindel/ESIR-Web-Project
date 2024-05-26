89e6efb1e822be34a756b186dffd7f508ad85270
Jérémy BINDEL <jerebind@outlook.com>
2024-05-26 14:41:05 +0200

    Add EmailModule as import

--- back/src/associations/associations.module.ts
+++ back/src/associations/associations.module.ts
@@ -6,9 +6,10 @@ import { AssociationsController } from './associations.controller';
 import { AssociationsService } from './associations.service';
 import { User } from '../users/user.entity';
 import { AssociationListener } from './association.listener';
+import { EmailModule } from '../email/email.module';
 
 @Module({
-    imports : [TypeOrmModule.forFeature([User, Association]), UsersModule],
+    imports : [TypeOrmModule.forFeature([User, Association]), UsersModule, EmailModule],
     controllers:[AssociationsController],
     providers: [AssociationsService, AssociationListener],
     exports: [AssociationsService]
--- back/src/users/users.module.ts
+++ back/src/users/users.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { EmailModule } from '../email/email.module';
 
 @Module({
-  imports : [TypeOrmModule.forFeature([User])],
+  imports : [TypeOrmModule.forFeature([User]), EmailModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService]
