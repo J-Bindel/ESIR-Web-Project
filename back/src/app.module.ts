1b5ab8b3c35ff566559b49699ddeda3d4bf190a8
Jérémy BINDEL <jerebind@outlook.com>
2024-06-23 17:30:30 +0530

    Add NestJS prometheus client

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { PrometheusModule } from "@willsoto/nestjs-prometheus";
 
 @Module({
   imports: [
@@ -17,7 +18,7 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
-    UsersModule, AssociationsModule, AuthModule, EventEmitterModule.forRoot()],
+    UsersModule, AssociationsModule, AuthModule, EventEmitterModule.forRoot(), PrometheusModule.register()],
   controllers: [],
   providers: [],
 })
